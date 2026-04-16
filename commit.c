e539465eb0e060de259147f3c7886159c5cb17ad
Soumya <soumyaproj@gmail.com>
2026-04-16 11:57:12 +0530

    Phase 4: improvements

--- commit.c
+++ commit.c
@@ -1,6 +1,7 @@
 // commit.c — Commit creation and history traversal
 //Phase 4 edit 1
 //Phase 4 edit 2
+//Phase 4 edit 3
 // Commit object format (stored as text, one field per line):
 //
 //   tree <64-char-hex-hash>
